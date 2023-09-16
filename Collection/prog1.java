deb280cc469bade115c8a3b27653403b6efcfacd
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-09-16 14:02:27 +0530

    Add files via upload

--- Collection/prog1.java
+++ Collection/prog1.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+import java.io.*;
+class prog1
+{
+	public static void main(String args[])
+	{
+		ArrayList al=new ArrayList();
+		al.add(new Integer(15));
+		al.add(new Integer(10));
+		al.add(new Integer(5));
+		al.add(new Integer(25));
+		System.out.println(al);
+		al.remove(new Integer(50));
+		System.out.println(al);
+		al.set(2,new Integer(30));
+		System.out.println(al);
+		System.out.println(al.get(1));
+	}
+}
