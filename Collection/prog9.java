77ecc0ba15132cfab1503ef188c39b67f1b3c502
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-09-19 19:01:53 +0530

    Add files via upload

---
+++ Collection/prog9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class prog9
+{
+    public static void main(String args[])
+    {
+        TreeSet ts=new TreeSet();
+        ts.add("B");
+        ts.add("D");
+        ts.add("A");
+        ts.add("E");
+        ts.add("C");
+        System.out.println(ts);
+    }
+}
