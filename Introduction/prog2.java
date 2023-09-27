c5795aa7c181f7ecc8ccb04f19445eccb5ef9f2e
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-09-27 20:09:19 +0530

    Add files via upload

---
+++ Introduction/prog2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class prog2
+{
+    public static void main(String args[])
+    {
+        int a, b;
+        a=10;
+        b=20;
+        int c =a+b;
+        System.out.println("Addition="+c);
+    }
+}
