a20abfa35dd9b3dfc8bf53cf5617abd84cb1d542
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-09-30 20:32:54 +0530

    Add files via upload

---
+++ Introduction/prog5.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class prog5
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=10;
+        b=20;
+        if(a>b)
+        System.out.println("a is maximum");
+        else
+        System.out.println("b is maximun");
+    }
+}
