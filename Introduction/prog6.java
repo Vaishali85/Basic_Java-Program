3d806503d9c9a0c981044fbe83c263fd16bab8d3
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-10-01 21:43:07 +0530

    Add files via upload

---
+++ Introduction/prog6.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class prog6
+{
+    public static void main(String args[])
+{
+   int a,b;
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter a & b");
+    a=Integer.parseInt(br.readLine());
+    b=Integer.parseInt(br.readLine());
+    int c=a+b;
+    System.out.println("Addition ="+c);
+}
+}
