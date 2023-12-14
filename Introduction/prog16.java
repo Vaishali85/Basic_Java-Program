41e071df90b1530676e68f656fcc27687aa128c4
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-12-14 20:41:45 +0530

    Add files via upload

---
+++ Introduction/prog16.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class prog16
+{
+    public static void main(String args[]) 
+    {
+        int a=9;
+        int b=6;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
