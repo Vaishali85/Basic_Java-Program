dbeef0d5c3c9fb2c8303c6232634ed0462f78e03
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-12-17 20:06:21 +0530

    Add files via upload

---
+++ Introduction/prog20.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class prog20
+{
+    public static void main(String args[])
+    {
+        int a= Integer.parseInt(args[0]);
+        int b= Integer.parseInt(args[1]);
+        int c= Integer.parseInt(args[2]);
+    }
+}
