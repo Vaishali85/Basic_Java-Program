e08a6dae0e5a0276b57089d256869625ae7f5b1b
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2023-12-18 19:44:14 +0530

    Add files via upload

---
+++ Introduction/prog21.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class prog21
+{
+    public static void main(String args[])
+    {
+        int day=Integer.parseInt(args[0]);
+        switch (day)
+        {
+            case 1: System.out.println("Sunday");
+                    break;
+            case 2: System.out.println("Monday");
+                    break;
+            case 3: System.out.println("Tuesday");
+                    break;
+            case 4: System.out.println("Wendsday");
+                    break;
+            case 5: System.out.println("Thursday");
+                    break;
+            case 6: System.out.println("Friday");
+                    break;
+            case 7: System.out.println("Saturday");
+                    break;
+            default: System.out.println("Invalid Input");
+        }
+    }
+}
