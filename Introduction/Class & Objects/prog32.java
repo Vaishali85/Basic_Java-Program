29bf43d8ea8d9009d26b5a806316fa53546b6bd1
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2024-03-06 20:23:46 +0530

    Add files via upload

--- Introduction/Class & Objects/prog32.java
+++ Introduction/Class & Objects/prog32.java
@@ -1,1 +1,20 @@
-
+import java.io.*;
+class Student
+{
+    int rno;
+    String name;
+    float perc;
+}
+class prog32
+{
+    public static void main(String args[])throws IOException
+    {
+        Student S1=new Student();
+        S1.rno=10;
+        S1.name="abc";
+        S1.perc=70;
+        System.out.println("Rno:" +S1.rno);
+        System.out.println("Name:" +S1.name);
+        System.out.println("Perc:" +S1.perc);
+    }
+}
