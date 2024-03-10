9371caa6183585c25ba113a6889076a38787db2b
Vaishali85 <89194466+Vaishali85@users.noreply.github.com>
2024-03-10 22:06:48 +0530

    Add files via upload

---
+++ Introduction/Class & Objects/prog37.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class Employee
+{
+    int eno;
+    String name;
+    double salary;
+    void acceptEmployee()
+{
+    eno=101;
+    name="xyz";
+    salary=20000;
+}
+    void printEmployee()
+    {
+        System.out.println("RNO:"+eno);
+        System.out.println("Name:"+name);
+        System.out.println("Salary:"+salary);
+    }
+}
+class prog37
+{
+    public static void main(String args[])
+    {
+        Employee e1=new Employee();
+        e1.acceptEmployee();
+        Employee e2=new Employee();
+        e2.acceptEmployee();
+        e1.printEmployee();
+        e2.printEmployee();
+    }
+}
